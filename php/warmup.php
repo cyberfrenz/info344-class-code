29a2a1c74a5a495813fdb285d6eecbc41a2cf003
Your Name <you@example.com>
2016-01-10 19:12:46 -0800

    did month thing

--- php/warmup.php
+++ php/warmup.php
@@ -1,6 +1,19 @@
 <?php 
+#1.
 $random = rand(1, 100);
 echo "your new random value is $random\n";
 
-$months = array();
+#2.
+$month = array_fill(0, 1, "start");
+
+for ($x = 1; $x <= 12; $x++) {
+	$blah = jdmonthname(($x*30), 1);
+	$month[($x-1)]= $blah;
+} 
+
+array_multisort($month);
+for ($x = 0; $x <=12; $x++) {
+	echo ("$month[$x] \n");
+}
+
 ?>
